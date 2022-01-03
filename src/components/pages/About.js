9d252addee9f2784e522e45670c26cb3b5179848
HAMAYOUNMASROOR <hamayounmasroor29@gmail.com>
2022-01-04 00:53:44 +0500

    Initial commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="container">
+      <div className="py-4">
+        <h1>About Page</h1>
+        <p className="lead">
+        React is a free and open-source front-end JavaScript library for building user interfaces based on UI components. It is maintained by Meta and a community of individual developers and companies. React can be used as a base in the development of single-page or mobile applications.
+        </p>
+
+        <p className="lead">
+        React is a free and open-source front-end JavaScript library for building user interfaces based on UI components. It is maintained by Meta and a community of individual developers and companies. React can be used as a base in the development of single-page or mobile applications.
+        </p>
+        <p className="lead">React is a free and open-source front-end JavaScript library for building user interfaces based on UI components. It is maintained by Meta and a community of individual developers and companies. React can be used as a base in the development of single-page or mobile applications.</p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
